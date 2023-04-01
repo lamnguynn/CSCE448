ec6f4de658e6862e5491a40697c8dde7d1b56159
Lam Nguyen <lance66nguyen@gmail.com>
2023-03-31 19:21:42 -0500

    general refactor. add function to validate model with test dataset and show eigenfaces

--- Final Project/pca.py
+++ Final Project/pca.py
@@ -1,10 +1,12 @@
+import random
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.datasets import fetch_olivetti_faces
+from sklearn.datasets import fetch_olivetti_faces, fetch_lfw_people
 from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 
 
@@ -80,7 +82,31 @@ def try_with_own_faces(pca, svm, x, y):
         plt.subplot(1, 2, 2)
         image = x[img_index]
         plt.imshow(image.reshape(64,64), cmap=plt.cm.gray)
-        plt.title('Predicted Image Stored in DB')
+        plt.title('Predicted Image of label ' + str(new_image_label))
+
+        plt.xticks([])
+        plt.yticks([])
+
+        plt.show()
+
+
+def try_with_dataset_faces(pca, svm, x, X_test_pca):
+    random_5 = X_test_pca[np.random.choice(len(X_test_pca), size=5, replace=False)]
+
+    for image in random_5:
+        new_image_label = svm.predict(image.reshape(1, -1))[0]
+        reverted_image = pca.inverse_transform(image).reshape(64,64)
+
+        plt.subplot(1, 2, 1)
+        plt.imshow(reverted_image, cmap='gray')
+        plt.title('Inputted Image')
+        plt.xticks([])
+        plt.yticks([])
+
+        plt.subplot(1, 2, 2)
+        image = x[new_image_label * 10]
+        plt.imshow(image.reshape(64, 64), cmap=plt.cm.gray)
+        plt.title('Predicted Image of label ' + str(new_image_label))
 
         plt.xticks([])
         plt.yticks([])
@@ -98,7 +124,7 @@ def plot_n_components(X_train, X_test, y_train, y_test):
     :param y_test: testing labels
     :return: None
     '''
-    n_components = [1, 3, 5, 10, 20, 40, 50]
+    n_components = [5, 10, 20, 40, 50, 100, 200]
     accuracies = []
     for n in n_components:
         pca = PCA(n_components=n)
@@ -118,9 +144,22 @@ def plot_n_components(X_train, X_test, y_train, y_test):
     plt.ylabel("Accuracy")
     plt.xlabel("Components")
     plt.xticks(n_components)
-    plt.plot(n_components, accuracies)
+    plt.plot(n_components, accuracies, marker='o')
     plt.show()
 
+
+def plot_eigen_faces(eigenfaces, titles, h, w, n_row=3, n_col=4):
+    plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
+    plt.subplots_adjust(bottom=0, left=0.01, right=0.99, top=0.90, hspace=0.35)
+
+    for i in range(n_row * n_col):
+        plt.subplot(n_row, n_col, i + 1)
+        plt.imshow(eigenfaces[i].reshape((h, w)), cmap=plt.cm.gray)
+        plt.title(titles[i], size=12)
+        plt.xticks(())
+        plt.yticks(())
+
+    plt.show()
 
 def main():
     # Training model with olivetti dataset and my own selfies
@@ -128,21 +167,27 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    pca = PCA(n_components=80)
+    pca = PCA(n_components=100)
     pca.fit(X_train)
 
     X_train_pca = pca.transform(X_train)
     X_test_pca = pca.transform(X_test)
 
-    svm = SVC(kernel='linear')
-    svm.fit(X_train_pca, y_train)
+    svm = SVC()
+    clf = GridSearchCV(svm, { 'C':[0.1,1,100],'kernel':['rbf','poly','sigmoid','linear'],'degree':[1,2,3,4],'gamma': [1, 0.1, 0.01, 0.001, 0.0001]})
+    clf.fit(X_train_pca, y_train)
+    print(clf.best_params_)
+    print('Accuracy:', clf.score(X_test_pca, y_test))
 
-    y_pred = svm.predict(X_test_pca)
-    accuracy = accuracy_score(y_test, y_pred)
-    print('Accuracy:', accuracy)
+    n_components = 100
+    eigenfaces = pca.components_.reshape((n_components, 64, 64))
+    eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
+    #plot_eigen_faces(eigenfaces, eigenface_titles, 64, 64)
+    #plot_n_components(X_train, X_test, y_train, y_test)
 
-    plot_n_components(X_train, X_test, y_train, y_test)
-    try_with_own_faces(pca, svm, X, y)
+    # Uncomment these to test
+    #try_with_own_faces(pca, clf, X, y)
+    #try_with_dataset_faces(pca, clf, X, X_test_pca)
 
 
 main()
